acb6a240e085ceae3c13d351cb0dbe0afb91ae40
tgkabi3009 <rezamaulana200@gmail.com>
2026-07-20 01:22:14 +0700

    Delete absensi_guru_apk directory

--- absensi_guru_apk/absensi_guru_apk/android/app/src/main/kotlin/com/absensi/guru/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.absensi.guru
-
-import io.flutter.app.FlutterApplication
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.plugins.firebase.messaging.FlutterFirebaseMessagingService
-
-class MainActivity : FlutterActivity()
